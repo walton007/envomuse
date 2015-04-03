c6c16efd8360f247b6dc5e26b33b1727daf0943a
walton.wang <walton.wang@sap.com>
2015-04-03 09:14:20 +0800

    i071628 JIRA: BEB- Description: add route

--- packages/custom/envomuse/server/routes/envomuse.js
+++ packages/custom/envomuse/server/routes/envomuse.js
@@ -1,16 +1,174 @@
 'use strict';
 
 /* jshint -W098 */
+var express = require('express'),
+   mean = require('meanio');
+
 // The Package is past automatically as first parameter
 module.exports = function(Envomuse, app, auth, database) {
+  //Set musicAssert
+  var config = mean.loadConfig();
+  app.use('/musicAssert', express.static(config.root + '/musicAssert'));
 
-  app.get('/envomuse/example/anyone', function(req, res, next) {
-    res.send('Anyone can access this');
+  var apiRouter = express.Router();
+  app.use('/api', apiRouter);
+
+  //Coming Jobs
+  apiRouter.route('/comingJobs')
+  .get(function(req, res, next) {
+    res.json([{id:1}, {id:2}]);
+  });
+  
+  apiRouter.route('/comingJobs/:comingJobId')
+  .get(function(req, res, next) {
+    res.json({id:2});
   });
 
-  app.get('/envomuse/example/auth', auth.requiresLogin, function(req, res, next) {
-    res.send('Only authenticated users can access this');
+  apiRouter.route('/comingJobs/:comingJobId/active')
+  .post(function(req, res, next) {
+    res.send(200);
   });
+  apiRouter.param('comingJobId', function(req, res, next, id){
+    req.comingJobs = null;
+    next();
+  }); 
+
+  //Jobs
+  apiRouter.get('/jobs', function(req, res, next) {
+    res.json([{id:1}, {id:2}]);
+  });
+
+  apiRouter.route('/jobs/:jobId')
+  .get(function(req, res, next) {
+    res.json({id:2});
+  })
+  .delete(function(req, res, next) {
+    res.send(200);
+  });
+  apiRouter.route('/jobs/:jobId/generateProgram')
+  .post(function(req, res, next) {
+    //generateProgram
+    res.json({
+      id: 100
+    });
+  });
+  apiRouter.param('jobId', function(req, res, next, id){
+    req.job = null;
+    next();
+  }); 
+
+  //Programs
+  apiRouter.get('/programs', function(req, res, next) {
+    res.json([{id:1}, {id:2}]);
+  });
+  apiRouter.route('/programs/:programId')
+  .get(function(req, res, next) {
+    res.json({id:2});
+  })
+  .delete(function(req, res, next) {
+    res.send(200);
+  });
+  apiRouter.route('/programs/:programId/bindSite')
+  .post(function(req, res, next) {
+    res.json({id:2});
+  });
+  apiRouter.param('programId', function(req, res, next, id){
+    req.program = null;
+    next();
+  }); 
+
+  //Songs
+  apiRouter.route('/songs')
+  .get(function(req, res, next) {
+    res.json([{id:1}, {id:2}]);
+  });
+  apiRouter.route('/songs/:songId')
+  .get(function(req, res, next) {
+    res.json([{id:1}]);
+  });
+  apiRouter.route('/songs/:songId/hqfile')
+  .get(function(req, res, next) {
+    res.redirect('/musicAssert/alphaEnc.mp3');
+  });
+  apiRouter.route('/songs/:songId/lqfile')
+  .get(function(req, res, next) {
+    res.redirect('/musicAssert/alpha.mp3');
+  });
+  apiRouter.param('songId', function(req, res, next, id){
+    req.program = null;
+    next();
+  }); 
+
+  app.route('/resource/alpha.mp3')
+  .get(function(req, res, next) {
+    res.json([{id:1}, {id:23}]);
+  });
+  app.route('/resource/alphaEnc.mp3')
+  .get(function(req, res, next) {
+    res.json([{id:1}, {id:4}]);
+  });
+
+  //Customers
+  apiRouter.route('/customers/')
+  .get(function(req, res, next) {
+    res.json([{id:1}, {id:2}]);
+  })
+  .post(function(req, res, next) {
+    res.json({id:2});
+  });
+  apiRouter.route('/customers/:customerId')
+  .get(function(req, res, next) {
+    res.json({id:2});
+  })
+  .put(function(req, res, next) {
+    res.json({id:2});
+  })
+  .delete(function(req, res, next) {
+    res.json(200);
+  });
+  apiRouter.param('customerId', function(req, res, next, id){
+    req.customer = null;
+    next();
+  }); 
+
+  //Sites
+  apiRouter.route('/sites/')
+  .get(function(req, res, next) {
+    res.json([{id:1}, {id:2}]);
+  })
+  .post(function(req, res, next) {
+    res.json({id:2});
+  });
+  apiRouter.route('/sites/:siteId')
+  .get(function(req, res, next) {
+    res.json({id:2});
+  })
+  .put(function(req, res, next) {
+    res.json({id:2});
+  })
+  .delete(function(req, res, next) {
+    res.json(200);
+  });
+  apiRouter.route('/sites/:siteId/bindLicense')
+  .post(function(req, res, next) {
+    res.send(200);
+  });
+  apiRouter.route('/sites/:siteId/license/activate')
+  .post(function(req, res, next) {
+    res.send(200);
+  });
+  apiRouter.route('/sites/:siteId/connectionLogs')
+  .get(function(req, res, next) {
+    res.json([{id:2}]);
+  });
+  apiRouter.param('siteId', function(req, res, next, id){
+    req.site = null;
+    next();
+  });
+
+  //ConnectionLogs For Music Player
+  // ConnectionLog
+
 
   app.get('/envomuse/example/admin', auth.requiresAdmin, function(req, res, next) {
     res.send('Only users with Admin role can access this');
